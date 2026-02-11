06ae9ab94117fe3cf89f7c72f5f53c5bd6279c54
PrasidBhusal10 <prasidbhusal07@gmail.com>
2026-02-11 16:35:50 -0600

    update

--- lab01/src/three_sum_sorted.c
+++ lab01/src/three_sum_sorted.c
@@ -1,8 +1,34 @@
 #include "three_sum_sorted.h"
 
 int three_sum_zero_sorted_one(const int* nums, int n, int* out_i, int* out_j, int* out_k) {
-    
-    // implement your code here
+    if (nums == NULL || n < 3) 
+    {
+        return 0;
+    }
 
+    for (int i = 0; i < n - 2; i++) {
+        int left = i + 1;
+        int right = n - 1;
+
+        while (left < right) {
+            long long sum = (long long)nums[i] + nums[left] + nums[right];
+
+            if (sum == 0) {
+                *out_i = i;
+                *out_j = left;
+                *out_k = right;
+                return 1; // Success
+            } 
+            else if (sum < 0) {
+                left++; // Increase the sum
+            } 
+            else {
+                right--; // Decrease the sum
+            }
+        }
+    }
+
+    return 0; // No triplet found
+}
     return 0;
 }
